fea3f80cdb6df9204d5ebd752a213638a74e4a61
ATMECS\ranjitha.selvam <ranjitha.selvam@atmecs.com>
2020-07-20 21:03:37 +0530

    chai

---
+++ git.ts
@@ -0,0 +1,6 @@
+export class FilePath {
+
+    public static readonly mainNavigation: any = 'locators/mainnavigationloc.properties';
+    public static readonly dashboard: any = 'locators/dashboardloc.properties';
+
+}
